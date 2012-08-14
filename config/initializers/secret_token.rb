c9904715e6ff0ed5f78f59f41ccdb07d7e0d0ed4
Aleksi Kervinen <pollyzoid@gmail.com>
2012-08-14 05:06:51 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nuudge::Application.config.secret_token = 'b4c74e43f32d1997379f187e39b7d5693a8d7b7d57535ee3182adef417452b417016d1c3274648d67c97b208497fa20adf1a9da56fc15dd83794a24b7d5270a3'
